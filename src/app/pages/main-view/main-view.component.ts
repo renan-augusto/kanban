aeddf6e63f53d255d1440478b23e1ea729b031eb
renan-augusto <renan_acm1992@hotmail.com>
2024-03-04 21:25:32 -0300

    construction(main-view) - drag and drop construction initialized

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+  DragDropModule,
+} from '@angular/cdk/drag-drop';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-main-view',
   standalone: true,
-  imports: [],
+  imports: [BrowserModule, DragDropModule],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.scss'
 })
-export class MainViewComponent {
+export class MainViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 
 }
